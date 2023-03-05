62b91d24245efa793b3a5903398b1bbada655529
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-03-06 00:17:46 +0900

    Update leadtime.rb

--- Formula/leadtime.rb
+++ Formula/leadtime.rb
@@ -1,8 +1,8 @@
 class LeadTime < Formula
   desc "caluculate PR lead time statistics on GitHub"
   homepage "https://github.com/nao1215/leadtime"
-  url "https://github.com/nao1215/leadtime/releases/download/v0.0.3/leadtime"
-  sha256 "1a6fe8a2953680578c3c7bc42236964447f80105b4dcabf7ffb5478aa57f2e58"
+  url "https://github.com/nao1215/leadtime/releases/download/v0.0.4/leadtime"
+  sha256 "0747f6ba153fcde0056aa2aa5e69ff9c39d01d26520dc80f44f2381e356a4b68"
 
   def install
     bin.install "leadtime"
