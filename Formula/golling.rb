7dc134d111fe57d9bea0b32b7515724a911cc5f4
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-03-08 20:00:13 +0900

    Update golling.rb

--- Formula/golling.rb
+++ Formula/golling.rb
@@ -1,8 +1,8 @@
 class Golling < Formula
   desc "Update golang to the latest version"
   homepage "https://github.com/nao1215/golling"
-  url "https://github.com/nao1215/golling/releases/download/v0.0.1/golling"
-  sha256 "997331c7e772c5ccc88f2bf7d031b5b7b045d26dddaeea6b2ad8c08abb5e9162"
+  url "https://github.com/nao1215/golling/releases/download/v0.0.2/golling"
+  sha256 "3f04499c962e26e845bca301945e5d577e520be5c92f49b25e571db7a16358d6"
 
   def install
     bin.install "golling"
