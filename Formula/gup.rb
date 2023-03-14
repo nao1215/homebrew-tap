1073ffe44e5fa6e89a995546ca9234ab44b08879
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-03-14 22:55:56 +0900

    Update gup.rb

--- Formula/gup.rb
+++ Formula/gup.rb
@@ -1,8 +1,8 @@
 class Gup < Formula
   desc "Update binaries installed by 'go install' with goroutines."
   homepage "https://github.com/nao1215/gup"
-  url "https://github.com/nao1215/gup/releases/download/v0.19.0/gup"
-  sha256 "d3784f97b65b5d1565c9bcdaee38e1586abe9865d970204af03fa525672fb0e3"
+  url "https://github.com/nao1215/gup/releases/download/v0.20.0/gup"
+  sha256 "2263e59b0b7149460bc10eb159742aa433cdcc8eb1bef204bb21b237d5d63c69"
 
   def install
     bin.install "gup"
