69f40ee03c15697dfc8738a163a62d820840c918
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-03-30 12:54:11 +0900

    Update jose.rb

--- Formula/jose.rb
+++ Formula/jose.rb
@@ -1,8 +1,8 @@
 class Jose < Formula
   desc "CLI tool for JOSE (JSON Object Signing and Encryption)"
   homepage "https://github.com/nao1215/jose"
-  url "https://github.com/nao1215/jose/releases/download/v0.0.2/jose"
-  sha256 "29670c48a4cca13f19142bbb3ac8ecd03dff2e701670f7cc0f4b49c28e2f817c"
+  url "https://github.com/nao1215/jose/releases/download/v0.0.3/jose"
+  sha256 "f05c4e97c5a1bffd6dde1513f9fd838a99e5a59e769c28bafc22782d3d1dd155"
 
   def install
     bin.install "jose"
